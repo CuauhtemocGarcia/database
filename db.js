aa7a5dc5b44cc0d644baabf8e20afffec6bb5b91
Tonatiuh cuauhtemoc garcia carrasco <ISC20350533@gmail.com>
2023-10-09 10:21:51 -0600

    database

---
+++ db.js
@@ -0,0 +1,20 @@
+const mysql = require('mysql');
+
+const config = {
+    host: 'localhost',
+    port: 3306,
+    database: 'backend',
+    user: 'root',
+    password:''
+    
+};
+
+const conn = mysql.createConnection(config);
+
+conn.connect((err) =>{
+    if (err) {
+        console.log("ERROR OCURRED WHILE CONNECTING TO MYSQL DATABASSE.", err);
+    } else {
+        console.log("CONNECTION WITH MYSQL DATABASE CREATED SUCCESSFULLY.");
+    }
+});
